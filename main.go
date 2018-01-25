ccb13ac6629a8bb8abaaa251669a0ffe4d4985bb
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2018-01-25 16:43:10 +0200

    hello, world!

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Specification struct {
+	Port int64 `default:"8080"`
+}
+
+func main() {
+	var s Specification
+	envconfig.MustProcess("", &s)
+
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+
+	port := strconv.FormatInt(s.Port, 10)
+	http.ListenAndServe(":"+port, r)
+}
