2ce95ba282243c4861dc062005451a5b59db4654
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2018-01-27 17:06:15 +0200

    set access control header

--- app.go
+++ app.go
@@ -41,5 +41,6 @@ func dashboardHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(b)
 }
